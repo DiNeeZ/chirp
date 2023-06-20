4f40f0d2ad1c32336c6bb0ba6952b20851add4a3
DiNeez <dneez86@gmail.com>
2023-06-20 15:09:40 +0300

    Add greeting

--- src/components/ui/authButton.tsx
+++ src/components/ui/authButton.tsx
@@ -12,12 +12,18 @@ const AuthButton = () => {
       </SignInButton>
     );
   } else {
+    console.log(user);
     return (
-      <SignOutButton>
-        <button className="mb-2 mr-2 rounded-lg bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-pink-300 dark:focus:ring-pink-800">
-          Sign Out
-        </button>
-      </SignOutButton>
+      <div className="flex flex-col items-center justify-center gap-8">
+        <h2 className="text-xl font-bold text-white">
+          Привет, {user.user.fullName}
+        </h2>
+        <SignOutButton>
+          <button className="mb-2 mr-2 rounded-lg bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-pink-300 dark:focus:ring-pink-800">
+            Sign Out
+          </button>
+        </SignOutButton>
+      </div>
     );
   }
 };
